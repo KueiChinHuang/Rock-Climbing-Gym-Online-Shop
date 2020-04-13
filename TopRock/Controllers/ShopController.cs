b41a8412e6ef73a4425b1a1b462fb0111432ba19
Kuei-Chin Huang <Kuei-Chin.Huang@MyGeorgian.ca>
2020-04-12 21:23:16 -0400

    Create ShopController and set up database connection

---
+++ TopRock/Controllers/ShopController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TopRock.Models;
+
+namespace TopRock.Controllers
+{
+    public class ShopController : Controller
+    {
+        private readonly toprockContext _context;
+
+        public ShopController(toprockContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
