119cafce053f74161fe8da43c823cfc8e812769c
Kuei-Chin Huang <Kuei-Chin.Huang@MyGeorgian.ca>
2020-04-29 09:08:13 -0400

    Add test project

---
+++ TopRockTest/HomeControllerTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TopRockTest
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
